a957a6e90ed6a61bfa21647193dca6944f58d902
Jérôme Lebel <jerome@fotonauts.com>
2011-06-03 04:47:14 +0200

    creating SocketServer.m

--- iProxy/Sources/GenericServer.h
+++ iProxy/Sources/GenericServer.h
@@ -40,30 +40,6 @@ typedef enum
 
 @end
 
-
-@interface SocketServer : GenericServer
-{
-	NSError *_lastError;
-	CFSocketRef _sockets[2];
-    CFRunLoopSourceRef _runLoopSource[2];
-    NSMutableDictionary *_connectionPerIP;
-    NSUInteger _connectionCount;
-}
-
-@property(readonly) NSUInteger connectionCount;
-@property(readonly) NSUInteger ipCount;
-
-- (NSError *)lastError;
-- (void)_setLastErrorWithMessage:(NSString *)message;
-- (void)_closeSocket;
-- (void)didOpenConnection:(NSDictionary *)info;
-- (void)didCloseConnection:(NSDictionary *)info;
-- (void)closeConnection:(NSDictionary  *)handle;
-- (void)newReceiveIncomingConnectionWithInfo:(NSDictionary *)info;
-- (void)socketCallbackWithSocket:(CFSocketRef)sock type:(CFSocketCallBackType)type address:(CFDataRef)address data:(const void *)data;
-
-@end
-
 @protocol ProxyServer
 
 + (NSString *)pacFilePath;
